158a8707baad6426f24b7eb7169b43f52fec18e0
jobemcamera <jobecamera@gmail.com>
2023-01-16 18:25:10 -0300

    função JS para troca de info na section recursos ok

--- assets/js/recursos.js
+++ assets/js/recursos.js
@@ -1,25 +1,33 @@
 const opcao = document.querySelectorAll('.recursos__opcao')
-const imagem = document.getElementsByClassName('recursos__imagem')
+const imagem = document.getElementById('recursos-imagem')
 const titulo = document.getElementById('recursos-titulo')
 const texto = document.getElementById('recursos-texto')
 
-opcao[0].onclick = function() {
-    imagem.src = '/assets/images/illustration-features-tab-1.svg';
-    titulo.innerText = 'Bookmark in one click';
-    texto.innerText = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.';
-    console.log('ok1')
+function propriedadesImagem(altura, largura) {
+    imagem.style.height = altura;
+    imagem.style.width = largura;
+    imagem.style.backgroundPosition = 'center';
+    imagem.style.backgroundSize = 'contain';
+    imagem.style.backgroundRepeat = 'no-repeat';
 }
 
-opcao[1].onclick = function() {
-    imagem.src = '/assets/images/illustration-features-tab-2.svg';
-    titulo.innerText = 'Intelligent search';
-    texto.innerText = 'Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.';
-    console.log('ok2')
-}
-
-opcao[2].onclick = function() {
-    imagem.src = '/assets/images/illustration-features-tab-3.svg';
-    titulo.innerText = 'Share your bookmarks';
-    texto.innerText = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.';
-    console.log('ok3')
-}
+opcao.forEach((element, index) => {
+    element.onclick = function() {
+        if (index == 0) {
+            imagem.style.background = "url('/assets/images/illustration-features-tab-1.svg')";
+            propriedadesImagem('30vh', '80vw')
+            titulo.innerText = 'Bookmark in one click';
+            texto.innerText = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.';
+        } else if (index == 1) {
+            imagem.style.background = "url('/assets/images/illustration-features-tab-2.svg')";
+            propriedadesImagem('30vh', '80vw')
+            titulo.innerText = 'Intelligent search';
+            texto.innerText = 'Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.';
+        } else if (index == 2) {
+            imagem.style.background = "url('/assets/images/illustration-features-tab-3.svg')";
+            propriedadesImagem('30vh', '80vw')
+            titulo.innerText = 'Share your bookmarks';
+            texto.innerText = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.';
+        }
+    }
+})
