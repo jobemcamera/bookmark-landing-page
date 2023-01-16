f597a3f9c72e3e80ac7735d7ddd1a6975a43c48d
jobemcamera <jobecamera@gmail.com>
2023-01-16 19:32:04 -0300

    estilização parcial dos titulos do recurso

--- assets/js/recursos.js
+++ assets/js/recursos.js
@@ -11,6 +11,16 @@ function propriedadesImagem(altura, largura) {
     imagem.style.backgroundRepeat = 'no-repeat';
 }
 
+/*
+function linhaFundo(element) {
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+    } else {
+        element.classList.add('active');
+    }
+    
+}
+*/
 opcao.forEach((element, index) => {
     element.onclick = function() {
         if (index == 0) {
@@ -18,16 +28,19 @@ opcao.forEach((element, index) => {
             propriedadesImagem('30vh', '80vw')
             titulo.innerText = 'Bookmark in one click';
             texto.innerText = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.';
+           // linhaFundo(element);
         } else if (index == 1) {
             imagem.style.background = "url('/assets/images/illustration-features-tab-2.svg')";
             propriedadesImagem('30vh', '80vw')
             titulo.innerText = 'Intelligent search';
             texto.innerText = 'Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.';
+           // linhaFundo(element);
         } else if (index == 2) {
             imagem.style.background = "url('/assets/images/illustration-features-tab-3.svg')";
             propriedadesImagem('30vh', '80vw')
             titulo.innerText = 'Share your bookmarks';
             texto.innerText = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.';
+           // linhaFundo(element);
         }
     }
 })
