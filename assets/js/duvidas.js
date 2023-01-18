efdbaa3951603e67337c36e5ef88a91795c81e1c
jobemcamera <jobecamera@gmail.com>
2023-01-18 18:08:15 -0300

    estilização parcial section duvidas + js function (acordeao)

---
+++ assets/js/duvidas.js
@@ -0,0 +1,14 @@
+const acordeao = document.querySelectorAll('.duvidas__acordeao--titulo')
+
+
+acordeao.forEach((element, index) => {
+    element.onclick = function () {
+        element.classList.toggle('active')
+        var panel = this.nextElementSibling
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null 
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px"
+        }
+    }
+})
